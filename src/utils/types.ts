c25179253d8b39be0bf00b9b32690718c3d55a5b
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 18:52:28 +0800

    Extract interfaces into types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,4 +4,19 @@ export interface Recipe {
   image: string;
   imageType: string;
   summary: string;
+  servings: number;
+  readyInMinutes: number;
+  spoonacularScore: number;
+}
+
+export interface TrendingCardProps {
+  index: number;
+  keyword: string;
+  title: string;
+  pictureUrl: string;
+}
+
+export interface Props {
+  recipe: Recipe;
+  id: number;
 }
