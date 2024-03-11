061b7cdd4dae8ff869cba2103a458d626e57048e
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 19:00:51 +0800

    Add trending data and passing of data to child component and refactor TrendingSection to use map function for rendering TrendingCards

--- src/components/HomePage/TrendingSection.tsx
+++ src/components/HomePage/TrendingSection.tsx
@@ -2,14 +2,41 @@ import React from "react";
 import TrendingCard from "./TrendingCard";
 
 const TrendingSection = () => {
+  const trendingData = [
+    {
+      keyword: "italian",
+      pictureUrl: "/italian_food.jpg",
+      title: "Italian Cuisine",
+    },
+    {
+      keyword: "chinese",
+      pictureUrl: "/chinese_food.jpg",
+      title: "Chinese Cuisine",
+    },
+    {
+      keyword: "spanish",
+      pictureUrl: "/spanish_food.jpg",
+      title: "Spanish Cuisine",
+    },
+    {
+      keyword: "indian",
+      pictureUrl: "/indian_food.jpg",
+      title: "Indian Cuisine",
+    },
+  ];
   return (
     <section>
-      <h2 className="text-2xl mt-4 mb-2 font-bold">Trending Collections</h2>
-      <div className="cards_container flex gap-4">
-        <TrendingCard index={0} />
-        <TrendingCard index={1} />
-        <TrendingCard index={2} />
-        <TrendingCard index={3} />
+      <h2 className="text-2xl mt-4 mb-2 font-bold">Trending Cuisines</h2>
+      <div className="cards_container flex justify-between gap-4">
+        {trendingData.map((data, index) => (
+          <TrendingCard
+            key={index}
+            index={index}
+            keyword={data.keyword}
+            title={data.title}
+            pictureUrl={data.pictureUrl}
+          />
+        ))}
       </div>
     </section>
   );
