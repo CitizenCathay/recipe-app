7c44d16c82355dc852d242a87ee2bf53a5363895
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 18:47:05 +0800

    Modify url for background image and move background image to public folder

--- src/components/HomePage/Hero.tsx
+++ src/components/HomePage/Hero.tsx
@@ -4,7 +4,7 @@ import Searchbar from "../SharedComponents/Searchbar";
 const Hero = () => {
   return (
     <section className="mt-8">
-      <div className="hero__container bg-[url('../assets/pasta.jpg')] bg-cover rounded-lg h-96 p-9">
+      <div className="hero__container bg-[url('/pasta.jpg')] bg-cover rounded-lg h-96 p-9">
         <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl text-whitePrimary">
           Find your next favourite recipe
         </h1>
