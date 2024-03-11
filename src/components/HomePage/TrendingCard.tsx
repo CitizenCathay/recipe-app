c29b653cc9539f7c16db8d628f17d7f7ba96f2fa
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 19:03:33 +0800

    Implement routing for TrendingCard, add useEffect hook to TrendingCard component and modify styles

--- src/components/HomePage/TrendingCard.tsx
+++ src/components/HomePage/TrendingCard.tsx
@@ -1,19 +1,21 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import cardImage from "../../assets/card-image.jpg";
-import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { TrendingCardProps } from "../../utils/types";
 import * as api from "../../utils/api";
-import { useState } from "react";
 
-interface TrendingCardProps {
-  index: number;
-}
-
-const TrendingCard: React.FC<TrendingCardProps> = ({ index }) => {
+const TrendingCard: React.FC<TrendingCardProps> = ({
+  index,
+  title,
+  keyword,
+  pictureUrl,
+}) => {
   const searchTerms = ["Italian", "Chinese", "Spanish", "Indian"];
   const [recipes, setRecipes] = useState([]);
   const searchTerm = searchTerms[index];
+  const router = useRouter(); // Initialize useRouter
 
   const handleSearch = async (
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
@@ -22,6 +24,7 @@ const TrendingCard: React.FC<TrendingCardProps> = ({ index }) => {
       const recipes = await api.searchTrending(searchTerm, 1);
       setRecipes(recipes);
       console.log(recipes);
+      router.push(`/search/${searchTerm}`);
     } catch (e) {
       console.log(e);
     }
@@ -29,20 +32,28 @@ const TrendingCard: React.FC<TrendingCardProps> = ({ index }) => {
     console.log("Search query:", searchTerm);
   };
 
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+    localStorage.setItem("searchTerm", searchTerm);
+    //console.log("Recipes stored in localStorage:", recipes);
+  }, [recipes, searchTerm]);
+
   return (
-    <Link
-      href={`/search/${searchTerm}`}
-      className="trending__card cursor-pointer group"
-      onClick={handleSearch}
-    >
-      <Image src={cardImage} alt="image of pasta" className="rounded-xl" />
+    <a className="trending__card cursor-pointer group" onClick={handleSearch}>
+      <Image
+        src={pictureUrl}
+        alt="image of pasta"
+        className="rounded-xl"
+        width={220}
+        height={220}
+      />
       <h3 className="font-semibold text-sm sm:text-base pt-1 group-hover:underline underline-offset-4">
-        Butternut Squash Soup
+        {title}
       </h3>
       <p className="trending__card-time text-sm sm:text-base no-underline">
         50+ recipes
       </p>
-    </Link>
+    </a>
   );
 };
 
