77bca3d2c0495764d168b7c91c17ca23d787257d
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 19:08:28 +0800

    Add basic layout for recipe page

---
+++ src/app/search/[searchResults]/[recipePage]/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section>
+        <img src="/pasta.jpg" alt="pasta"></img>
+        <h1>Penne Alla Vodka</h1>
+      </section>
+      <section>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui in
+          ipsam, rem maiores inventore iste modi! Voluptas, natus sit! Magni
+          eius soluta aut tempore nisi minima vero eum culpa reiciendis?
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
