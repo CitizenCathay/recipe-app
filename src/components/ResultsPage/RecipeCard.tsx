43afdf511b178c710517a76080e75d8e0fdb8c0a
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-11 18:57:05 +0800

    Implement routing for individual recipes, adjust truncatedSummary to remove tags remove punctuation at end of summary and adjusted styles

--- src/components/ResultsPage/RecipeCard.tsx
+++ src/components/ResultsPage/RecipeCard.tsx
@@ -1,25 +1,54 @@
 import React from "react";
-import { Recipe } from "../../utils/types";
-import Image from "next/image";
+import { Props } from "../../utils/types";
+import { useRouter } from "next/navigation";
 
-interface Props {
-  recipe: Recipe;
-}
-
-const RecipeCard = ({ recipe }: Props) => {
+const RecipeCard = ({ recipe, id }: Props) => {
+  const storedSearchTerm = localStorage.getItem("searchTerm");
+  const router = useRouter(); // Initialize useRouter
+  const handleClick = () => {
+    console.log({ id });
+    console.log({ storedSearchTerm });
+    // Navigate to the dynamic recipe page for the clicked recipe
+    if (id && storedSearchTerm) {
+      router.push(`/search/${storedSearchTerm}/${id}`);
+    } else {
+      console.error("Missing id or stored search term.");
+    }
+  };
   const summary = recipe.summary
     .replace(/<br\s*\/?>/gm, "")
-    .replace(/<\/?b\s*\/?>/g, ""); // Remove line break and bold tags
-  const truncatedSummary = summary.split(" ").slice(0, 15).join(" "); // Limit summary to 15 words
+    .replace(/<\/?b\s*\/?>/g, "") // Remove line break and bold tags
+    .replace(/<\/?a\s*\/?>/g, ""); // Remove anchor tags
+
+  const words = summary.split(" ");
+  let lastWordIndex = Math.min(14, words.length - 1); // Ensure we don't exceed the number of words in the summary
+  let lastWord = words[lastWordIndex]; // Get the 15th word or the last word if the summary is shorter
+
+  if (lastWord) {
+    lastWord = lastWord.replace(/[,.?]$/, ""); // Remove periods and commas from the last word
+  }
+
+  const truncatedSummary =
+    words.slice(0, 14).join(" ") + (lastWord ? " " + lastWord : ""); // Limit summary to 15 words
+
   return (
-    <div className="recipe__card mb-8 w-80 rounded-t-lg mx-auto sm:w-60 px-2">
+    <div
+      className="recipe__card cursor-pointer mb-10 sm:mb-8 w-80 sm:w-64 md:w-full rounded-lg mx-auto shadow-md group"
+      onClick={handleClick}
+    >
       <img
         src={recipe.image}
         alt="image of recipe"
-        className="w-auto rounded-t-lg"
+        className="w-full rounded-t-lg"
       ></img>
-      <h2 className="font-semibold">{recipe.title}</h2>
-      <p className="leading-5">{truncatedSummary}...</p>
+      <h2 className="font-semibold text-lg px-4 pt-2 group-hover:underline underline-offset-4">
+        {recipe.title}
+      </h2>
+      <p className="px-4 pb-2">
+        {recipe.readyInMinutes} mins ·{" "}
+        {(recipe.spoonacularScore / 20).toFixed(2)} ★
+      </p>
+      <p className="leading-5 px-4 pb-4">{truncatedSummary}...</p>
     </div>
   );
 };
