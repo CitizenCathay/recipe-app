c2fc1be89f8d3e9d6b20f514787afc120da0436f
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-12 19:24:06 +0800

    Add addtional params to interface

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,11 +2,39 @@ export interface Recipe {
   id: number;
   title: string;
   image: string;
-  imageType: string;
   summary: string;
   servings: number;
+  cookingMinutes: number;
+  preparationMinutes: number;
   readyInMinutes: number;
+  cuisines: Array<string>;
+  dishTypes: Array<string>;
   spoonacularScore: number;
+  sourceName: string;
+  sourceUrl: string;
+  cheap: boolean;
+  glutenFree: boolean;
+  vegetarian: boolean;
+  vegan: boolean;
+  dairyFree: boolean;
+  nutrition: {
+    nutrients: {
+      amount: string;
+      name: string;
+      unit: string;
+    }[];
+  };
+  analyzedInstructions?: {
+    name: string;
+    steps: {
+      number: number;
+      step: string;
+      length: {
+        number: number;
+        units: string;
+      };
+    }[];
+  };
 }
 
 export interface TrendingCardProps {
