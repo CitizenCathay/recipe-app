5b4143e81c43053eb19d8a2437dada0495d23816
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-14 20:17:02 +0800

    Move component from SharedComponents folder to HomePage folder and remove debugging console.log statements

---
+++ src/components/HomePage/Searchbar.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import * as api from "../../utils/api";
+
+const Searchbar = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [recipes, setRecipes] = useState([]);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter(); // Initialize useRouter
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+    localStorage.setItem("searchTerm", searchTerm);
+  }, [recipes, searchTerm]);
+
+  const handleFormSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes);
+
+      router.push(`/search/${searchTerm}`);
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (inputRef.current) {
+      inputRef.current.blur();
+    }
+  };
+
+  return (
+    <form
+      className="bg-whitePrimary w-min flex pl-3 pr-1 py-1 rounded-md gap-2 shadow-xl"
+      onSubmit={handleFormSubmit}
+    >
+      <input
+        type="text"
+        placeholder="Search for recipes..."
+        className="outline-none text-sm sm:text-lg"
+        value={searchTerm}
+        onChange={handleInputChange}
+        ref={inputRef} // Assigning ref to the input element
+        id="searchInput"
+        name="searchInput"
+        required
+      />
+      <button
+        type="submit"
+        className="bg-primary px-6 py-1 sm:py-2 rounded-md shadow-xl font-semibold active:bg-secondary"
+      >
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
