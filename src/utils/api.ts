c9e176ed0e09fa515550ffc504e7b29e55012f2a
CitizenCathay <89077454+CitizenCathay@users.noreply.github.com>
2024-03-15 03:15:52 +0800

    Update base URL to vercel backend deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
 export const searchRecipes = async (searchTerm: string, page: number) => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+  const baseUrl = new URL(
+    "https://recipe-backend-2liy9r5vm-kurais-projects-2013c57e.vercel.app/api/recipes/search"
+  );
   baseUrl.searchParams.append("searchTerm", searchTerm);
   baseUrl.searchParams.append("page", String(page));
 
@@ -12,7 +14,9 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
 };
 
 export const searchTrending = async (searchCuisine: string, page: number) => {
-  const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+  const baseUrl = new URL(
+    "https://recipe-backend-2liy9r5vm-kurais-projects-2013c57e.vercel.app/api/recipes/search"
+  );
   baseUrl.searchParams.append("searchTerm", searchCuisine);
   baseUrl.searchParams.append("page", String(page));
 
